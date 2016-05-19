a37c255addb5959dc43641b1fae642baf5c864eb
1031066280@qq.com <1031066280@qq.com>
2016-05-19 14:21:43 +0800

    1

---
+++ app/src/main/java/liuliu/hotel/activity/SetIpActivity.java
@@ -0,0 +1,20 @@
+package liuliu.hotel.activity;
+
+import liuliu.hotel.R;
+import liuliu.hotel.base.BaseActivity;
+
+/**
+ * 设置IP
+ * Created by Administrator on 2016/5/19.
+ */
+public class SetIpActivity extends BaseActivity {
+    @Override
+    public void initViews() {
+        setContentView(R.layout.activity_set_ip);
+    }
+
+    @Override
+    public void initEvents() {
+
+    }
+}
