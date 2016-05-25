ac9303d6c97167b72c96056cd071fd3177b7b17d
1031066280@qq.com <1031066280@qq.com>
2016-05-25 13:40:15 +0800

    1

---
+++ app/src/main/java/liuliu/hotel/ui/frag/SearchFragment.java
@@ -0,0 +1,18 @@
+package liuliu.hotel.ui.frag;
+
+import liuliu.hotel.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2016/5/24.
+ */
+public class SearchFragment extends BaseFragment {
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initEvents() {
+
+    }
+}
